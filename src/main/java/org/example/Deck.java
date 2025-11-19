d8e3aad049268921bf1e3547a1462a7c4571f317
dylankothari <dylan.kothari@menloschool.org>
2025-11-19 13:58:50 -0800

    Worked on the deck class, but still need to work on the constructor.

--- src/main/java/org/example/Deck.java
+++ src/main/java/org/example/Deck.java
@@ -1,4 +1,27 @@
-package org.example;
-
 public class Deck {
+    private ArrayList<Integer> cards;
+    private int cardsLeft;
+    public Deck(int[] ranks, String[] suits, double[] pointValues)
+    {
+        this.cards = new ArrayList<>();
+
+
+
+
+    }
+    public boolean isEmpty()
+    {
+        if(cardsLeft == 0)
+        {
+            return true;
+        }
+        return false;
+
+    }
+
+    public int getCardsLeft() {
+        return cardsLeft;
+    }
+    public
+
 }
